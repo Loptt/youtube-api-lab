3d7c26a651850f954ea1420521552d20ebb788e6
Carlos Estrada <carlos.deg02@hotmail.com>
2020-01-20 20:42:04 -0600

    Finished js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-function fetchVideos(term) {
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${term}&key=AIzaSyDxk8D7UvK5M76vNg5n2p-t7hsxW5h7yBA`;
+let currentTerm = "";
+
+function fetchVideos(term, token = "") {
+    let url = "";
+    if (token === "") {
+        url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${term}&key=AIzaSyDxk8D7UvK5M76vNg5n2p-t7hsxW5h7yBA`;
+    } else {
+        url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${term}&key=AIzaSyDxk8D7UvK5M76vNg5n2p-t7hsxW5h7yBA&pageToken=${token}`;
+    }
 
     $.ajax({
         url: url,
@@ -7,7 +14,7 @@ function fetchVideos(term) {
         dataType: "json",
         success: function(responseJSON) {
             console.log(responseJSON);
-            //displayResults(responseJSON);
+            displayResults(responseJSON);
         },
         error: function(err) {
             console.log(err);
@@ -16,19 +23,47 @@ function fetchVideos(term) {
 }
 
 function displayResults(responseJSON) {
-    let articles = responseJSON.articles;
+    let videos = responseJSON.items;
+    let nextPageToken = responseJSON.nextPageToken;
+    let prevPageToken = responseJSON.prevPageToken;
 
     $('#jsResult').html("");
     
-    articles.forEach((article) => {
+    videos.forEach((video) => {
         $('#jsResult').append(`
-            <div class="article">
-                <h1>${article.title}</h1>
-                <img src="${article.urlToImage}">
-                <h4>By: ${article.author}</h4>
-                <p>${article.description}</p>
+            <div class="video">
+                <a target="_blank" href="https://www.youtube.com/watch?v=${video.id.videoId}">
+                    <h2>${video.snippet.title}</h2>
+                    <img class="thumbnail" src="${video.snippet.thumbnails.high.url}">
+                </a>
             </div>
+            <div class="divider"></div>
         `);
+    });
+
+    $('#buttons').html("");
+
+    if (prevPageToken != null) {
+        $('#buttons').append(
+            `<button class="btn btn-primary" id="prevButton" value="${prevPageToken}">Previous</button>`
+        );
+    } 
+    if (nextPageToken != null) {
+        $('#buttons').append(
+            `<button class="btn btn-primary" id="nextButton" value="${nextPageToken}">Next</button>`
+        );
+    } 
+}
+
+function watchButtons() {
+    $('#buttons').on('click', '#nextButton', function(event) {
+        fetchVideos(currentTerm, $(this).val());
+        scroll(0,0);
+    });
+
+    $('#buttons').on('click', '#prevButton', function(event) {
+        fetchVideos(currentTerm, $(this).val());
+        scroll(0,0);
     });
 }
 
@@ -36,6 +71,7 @@ function watchForm() {
     $('#form').on('submit', function(event) {
         event.preventDefault();
         let term = $('#term').val();
+        currentTerm = term;
 
         if (term == "") {
             console.log("Empty term");
@@ -45,11 +81,11 @@ function watchForm() {
         fetchVideos(term);
         $('#term').val("");
     });
-
 }
 
 function init() {
     watchForm();
+    watchButtons();
 }
 
 init();
